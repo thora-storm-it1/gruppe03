bd8ba8e2cec65dafe5574629797218c92974e5c5
Tord Sætermo <tord.saetermo@users.noreply.github.com>
2019-03-12 08:54:15 +0100

    endret menyen til php

--- nettsider/kamper.php
+++ nettsider/kamper.php
@@ -20,14 +20,14 @@
       </div>
       <div class="meny">
         <ul>
-          <li><a class="meny_active" href="#home"> HJEM </a></li>
-          <li><a href="nettsider/nyheter.php"> NYHETER </a></li>
-          <li><a href="nettsider/tropp.php"> TROPP </a></li>
-          <li><a href="nettsider/sponsorer.php"> SPONSORER </a></li>
-          <li><a href="nettsider/forum.php"> FORUM </a></li>
-          <li><a href="nettsider/kamper.php"> KAMPER </a></li>
-          <li><a href="nettsider/tabell.php"> TABELL </a></li>
-          <li><a href="nettsider/om_rbk.php"> OM RBK </a></li>
+          <li><a href="index.html"> HJEM </a></li>
+          <li><a href="nyheter.html"> NYHETER </a></li>
+          <li><a href="tropp.html"> TROPP </a></li>
+          <li><a href="sponsorer.html"> SPONSORER </a></li>
+          <li><a href="forum.html"> FORUM </a></li>
+          <li><a class="meny_active"> KAMPER </a></li>
+          <li><a href="tabell.html"> TABELL </a></li>
+          <li><a href="om_rbk.html"> OM RBK </a></li>
         </ul>
       </div>
       <div class="innhold">
@@ -43,83 +43,10 @@
           Aliquam pretium massa ac tempus mollis. Suspendisse vel lacus dolor. Donec vulputate viverra magna, ac accumsan ante pretium at. Donec auctor sagittis enim at dapibus. Aenean tempor sodales ante, ullamcorper feugiat urna fermentum nec. Mauris eu ultrices libero. Nulla quis volutpat mauris. Proin consectetur justo enim, non mollis augue feugiat non. Etiam malesuada erat et turpis fringilla iaculis. Vestibulum nec consequat tortor. Proin dignissim consequat sagittis. Praesent porttitor a mauris non mattis. Sed ultricies, augue in efficitur feugiat, nulla quam consectetur enim, ac pretium libero tellus non leo. Vivamus mollis, arcu vitae consequat gravida, quam arcu sagittis urna, eu viverra magna lorem id nunc.
         </p>
 
-<!-- Lysbildekode fra w3schools (https://www.w3schools.com/howto/howto_js_slideshow.asp) slutter -->
-        <!-- Slideshow container -->
-        <div class="slideshow-container">
-
-          <!-- Full-width images with number and caption text -->
-          <div class="mySlides fade">
-            <div class="numbertext">1 / 3</div>
-            <img src="bilder/marbella.jpg" style="width:100%">
-            <div class="text"> Rosenborg på treningsleir i Marbella, Spania, denne uken for å gjøre seg klare før sesongstart. Les mer <a href="nyheter.html">her</a> . Foto: ROSENBORG </div>
-          </div>
-
-          <div class="mySlides fade">
-            <div class="numbertext">2 / 3</div>
-            <img src="bilder/ranheim.jpg" style="width:100%">
-            <div class="text"> Rosenborg tapte treningskampen mot Ranheim torsdag 28. februar. Les mer <a href="nyheter.html">her</a> . Foto: EIRIK EVENSEN </div>
-          </div>
-
-          <div class="mySlides fade">
-            <div class="numbertext">3 / 3</div>
-            <img src="bilder/brann.jpg" style="width:100%">
-            <div class="text"> Neste treningskamp for Rosenborg blir mot Brann den 10. mars.  Les mer <a href="nyheter.html">her</a> . Foto: ARNE RISTESUND </div>
-          </div>
-
-          <!-- Next and previous buttons -->
-          <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-          <a class="next" onclick="plusSlides(1)">&#10095;</a>
-        </div>
-        <br>
-
-        <!-- The dots/circles -->
-        <div style="text-align:center">
-          <span class="dot" onclick="currentSlide(1)"></span>
-          <span class="dot" onclick="currentSlide(2)"></span>
-          <span class="dot" onclick="currentSlide(3)"></span>
-        </div>
-
-        <script>
-          var slideIndex = 1;
-          showSlides(slideIndex);
-
-          // Next/previous controls
-          function plusSlides(n) {
-            showSlides(slideIndex += n);
-          }
-
-          // Thumbnail image controls
-          function currentSlide(n) {
-            showSlides(slideIndex = n);
-          }
-
-          function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("dot");
-            if (n > slides.length) {slideIndex = 1}
-            if (n < 1) {slideIndex = slides.length}
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex-1].style.display = "block";
-            dots[slideIndex-1].className += " active";
-          }
-        </script>
-<!-- Lysbildekode fra w3schools (https://www.w3schools.com/howto/howto_js_slideshow.asp) slutter -->
 
       </div>
       <div class="footer">
-        <h4> Dette er en UOFFISIEL Fanside laget som et skoleprosjekt. </h4>
-        <br>
-        <h4>
-          KONTAKT OSS: <br>
-          TLF: <br>
-          EPOST: <br>
-        </h4>
+        <h1> Dette er en UOFFISIEL Fanside laget som et skoleprosjekt. </h1>
       </div>
     </div>
   </body>
