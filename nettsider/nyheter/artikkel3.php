a1b0f1f2bfa139ddf1d1afb099d51887c304f90d
jakob1604 <48098435+jakob1604@users.noreply.github.com>
2019-03-15 13:29:09 +0100

    endret artikkler

--- nettsider/nyheter/artikkel3.php
+++ nettsider/nyheter/artikkel3.php
@@ -1,24 +1,51 @@
 <!doctype.html>
 <html>
-<head>
-<link rel="stylesheet" href="../styles/stilark.css">
-<meta charset="utf-8">
-<title> Rosenborg Ballklub </title>
-<link rel="icon" href="../bilder/rosenborg.png">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <title></title>
-</head>
-<body>
+  <head>
+    <link rel="stylesheet" href="../styles/stilark.css">
+    <meta charset="utf-8">
+    <title> Rosenborg Ballklub </title>
+    <link rel="icon" href="../bilder/rosenborg.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="innpakning">
+      <div class="header">
+        <div class="logo">
+          <img class="logo1" src="../bilder/rosenborg.png" width="200px" height="200px">
+        </div>
+        <div class="title">
+          <h1> ROSENBORG BALLKLUB </h1>
+        </div>
+      </div>
+      <div class="meny">
+        <ul>
+          <li><a href="../index.php"> HJEM </a></li>
+          <li><a class="meny_active"> NYHETER </a></li>
+          <li><a href="tropp.php"> TROPP </a></li>
+          <li><a href="sponsorer.php"> SPONSORER </a></li>
+          <li><a href="tabell.php"> TABELL/KAMPER </a></li>
+          <li><a href="om_rbk.php"> OM RBK </a></li>
+        </ul>
+      </div>
+      <div class="innhold">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum enim lorem. Praesent ornare ipsum turpis, id congue libero tempus sit amet. Nam rhoncus facilisis elit a facilisis. In vitae risus ac orci hendrerit dictum. Aliquam fermentum orci in augue tempus, nec bibendum tellus dapibus. Sed sollicitudin, magna eu hendrerit condimentum, risus ligula auctor mauris, in fermentum arcu nisi non dolor. In accumsan quis dolor ut cursus. Sed porttitor odio sed ullamcorper luctus. Vivamus pulvinar finibus elit. Fusce metus metus, varius non pulvinar porta, placerat placerat nisi. Nam sed lorem sed urna imperdiet efficitur. Vestibulum a sagittis tortor.
 
-<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut tincidunt turpis. In ultricies augue eu vehicula vulputate. Morbi aliquam bibendum magna et porta. Integer eu iaculis urna. Sed ullamcorper augue id semper interdum. Donec lectus orci, iaculis eu egestas eu, aliquet vel lacus. Aenean posuere vitae dui at ornare. Nunc egestas enim lorem, sed vulputate nibh volutpat nec. Pellentesque aliquet leo non vulputate fringilla. Maecenas elementum placerat mi. Proin faucibus neque sed dolor consequat sollicitudin. Mauris molestie elementum neque, at pharetra augue ullamcorper sed. Vivamus commodo egestas lorem ac porttitor. Nullam vel convallis magna, at accumsan diam. Nullam nec hendrerit eros.
+          In in volutpat dui. In in nunc volutpat, lobortis est eleifend, blandit lorem. Etiam placerat lectus mollis lorem scelerisque, ut consectetur metus vulputate. Quisque cursus, lacus at facilisis tempus, diam ante lobortis risus, quis elementum ante augue a enim. Suspendisse fringilla in risus id ultricies. Integer iaculis est nec porttitor semper. Aenean rutrum, leo ut finibus feugiat, nunc ipsum porttitor felis, eu viverra nibh quam ut nibh. Integer eget interdum urna. Cras at nunc convallis sapien viverra bibendum a in leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
 
-In vitae lacinia velit. Proin rhoncus dui eget arcu aliquam egestas. Integer in dui sem. Mauris vitae massa lectus. In in aliquam nisi. Quisque laoreet vestibulum nibh, vitae interdum lectus tristique sit amet. Pellentesque suscipit tortor orci, quis venenatis ligula efficitur nec. Suspendisse potenti. Nam nec magna faucibus ligula ornare condimentum eget vitae est. Suspendisse nisi nunc, lacinia dictum nisl tristique, porttitor vehicula enim. Ut orci dui, pulvinar in vulputate vel, mollis eu urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Quisque ullamcorper ut libero pulvinar imperdiet. Integer consequat justo eget nunc fringilla, consequat viverra lectus varius. Donec erat ligula, pellentesque eu dolor ac, scelerisque consectetur mi. Morbi ac laoreet nisi. In aliquam ultrices pellentesque. Cras aliquam quam consequat tincidunt tempus. Sed ut luctus lorem, ut auctor quam. Sed id elit sapien. Mauris consequat ullamcorper lacus, sodales fringilla leo euismod eget. Aliquam luctus nulla eget varius eleifend. Vivamus convallis nulla vel urna aliquam, convallis scelerisque justo cursus.
 
-Aenean et tortor ornare, imperdiet ipsum in, mattis arcu. Donec vel lacus sed dolor molestie malesuada. Sed accumsan leo urna, sed pharetra nisl mattis ut. Donec id odio dolor. Mauris non lacus congue, blandit mauris a, blandit eros. Phasellus fermentum, sapien quis feugiat consequat, augue massa tempus orci, iaculis maximus mauris velit ac urna. Ut laoreet euismod tortor et pretium. Suspendisse sollicitudin consectetur porta. Mauris at auctor arcu. Etiam lacinia interdum diam sit amet interdum. Proin aliquam auctor augue, at facilisis purus volutpat id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie, tortor eget malesuada vehicula, massa ipsum pretium nisi, ut ultricies felis velit sit amet leo. Phasellus quis ornare odio.
+          Quisque feugiat leo ut nisl facilisis ornare. Integer gravida purus eget diam dictum finibus. Integer vel lacus eget nunc hendrerit tincidunt. Suspendisse ultricies arcu vitae odio cursus ullamcorper. Praesent fermentum, urna in aliquet pretium, dolor magna vulputate neque, ut finibus ex tortor ut orci. Aliquam et cursus ipsum, placerat pharetra nisl. Quisque sodales cursus finibus. Quisque vehicula lorem est, sit amet scelerisque arcu volutpat quis. Praesent luctus quam a nulla convallis, vel vehicula quam auctor. Aliquam fermentum diam bibendum ligula condimentum pellentesque. Nunc eget nisl elit. Etiam et luctus mi. Sed finibus pretium purus, in varius dui pharetra vel. Morbi id quam mauris. Vivamus elementum ornare enim nec posuere.
 
-Nullam massa erat, ultricies in tellus quis, pulvinar sollicitudin libero. Aliquam interdum aliquet vehicula. Quisque rhoncus cursus enim a sollicitudin. Phasellus vehicula dictum diam, ut tincidunt purus laoreet vel. Nulla in iaculis odio. Etiam a justo pharetra, ultricies elit id, aliquam tellus. Suspendisse ut lectus ut augue dapibus auctor eget eget diam.
+          Aliquam pretium massa ac tempus mollis. Suspendisse vel lacus dolor. Donec vulputate viverra magna, ac accumsan ante pretium at. Donec auctor sagittis enim at dapibus. Aenean tempor sodales ante, ullamcorper feugiat urna fermentum nec. Mauris eu ultrices libero. Nulla quis volutpat mauris. Proin consectetur justo enim, non mollis augue feugiat non. Etiam malesuada erat et turpis fringilla iaculis. Vestibulum nec consequat tortor. Proin dignissim consequat sagittis. Praesent porttitor a mauris non mattis. Sed ultricies, augue in efficitur feugiat, nulla quam consectetur enim, ac pretium libero tellus non leo. Vivamus mollis, arcu vitae consequat gravida, quam arcu sagittis urna, eu viverra magna lorem id nunc.
+        </p>
 
-Phasellus eleifend eros id lorem laoreet luctus sit amet eu ex. Phasellus imperdiet diam congue risus pulvinar, at porta metus pulvinar. Aliquam egestas molestie nunc, eget bibendum urna porttitor eget. Vestibulum sed ultrices libero. Aenean sagittis purus a scelerisque feugiat. Proin velit lacus, fringilla ac tristique in, ullamcorper sed sem. In nec viverra dolor. Pellentesque rutrum magna eu ante scelerisque, in rutrum justo pulvinar. Fusce molestie orci urna, id condimentum metus tincidunt et. Duis condimentum ornare rhoncus. Quisque quis luctus est. Ut dignissim facilisis condimentum. Ut posuere nec lacus non pellentesque. Pellentesque gravida diam eu lacus pulvinar volutpat in sed nulla. Nam sit amet nisl in magna sodales mollis. Vestibulum massa nibh, hendrerit eu nibh vel, molestie eleifend quam.</h3>
-</body>
+
+      </div>
+      <div class="footer">
+        <h1> Dette er en UOFFISIEL Fanside laget som et skoleprosjekt. </h1>
+      </div>
+    </div>
+  </body>
 </html>
