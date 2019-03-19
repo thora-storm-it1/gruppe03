8ec068fee333ab24bc8f18404e30de3a017d3c2d
jakob1604 <48098435+jakob1604@users.noreply.github.com>
2019-03-19 08:28:32 +0100

    kommentarfelt
    + slettet noen databaser

--- nettsider/nyheter/artikkel1.php
+++ nettsider/nyheter/artikkel1.php
@@ -1,10 +1,17 @@
+
+
+<?php
+include '../../scripts/dph.inc.php';
+include '../../scripts/comments.inc.php';
+ ?>
+
 <!doctype.html>
 <html>
   <head>
-    <link rel="stylesheet" href="../styles/stilark.css">
+    <link rel="stylesheet" href="../../styles/stilark.css">
     <meta charset="utf-8">
     <title> Rosenborg Ballklub </title>
-    <link rel="icon" href="../bilder/rosenborg.png">
+    <link rel="icon" href="../../bilder/rosenborg.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   </head>
@@ -29,19 +36,87 @@
         </ul>
       </div>
       <div class="innhold">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum enim lorem. Praesent ornare ipsum turpis, id congue libero tempus sit amet. Nam rhoncus facilisis elit a facilisis. In vitae risus ac orci hendrerit dictum. Aliquam fermentum orci in augue tempus, nec bibendum tellus dapibus. Sed sollicitudin, magna eu hendrerit condimentum, risus ligula auctor mauris, in fermentum arcu nisi non dolor. In accumsan quis dolor ut cursus. Sed porttitor odio sed ullamcorper luctus. Vivamus pulvinar finibus elit. Fusce metus metus, varius non pulvinar porta, placerat placerat nisi. Nam sed lorem sed urna imperdiet efficitur. Vestibulum a sagittis tortor.
-
-          In in volutpat dui. In in nunc volutpat, lobortis est eleifend, blandit lorem. Etiam placerat lectus mollis lorem scelerisque, ut consectetur metus vulputate. Quisque cursus, lacus at facilisis tempus, diam ante lobortis risus, quis elementum ante augue a enim. Suspendisse fringilla in risus id ultricies. Integer iaculis est nec porttitor semper. Aenean rutrum, leo ut finibus feugiat, nunc ipsum porttitor felis, eu viverra nibh quam ut nibh. Integer eget interdum urna. Cras at nunc convallis sapien viverra bibendum a in leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-
-          Quisque ullamcorper ut libero pulvinar imperdiet. Integer consequat justo eget nunc fringilla, consequat viverra lectus varius. Donec erat ligula, pellentesque eu dolor ac, scelerisque consectetur mi. Morbi ac laoreet nisi. In aliquam ultrices pellentesque. Cras aliquam quam consequat tincidunt tempus. Sed ut luctus lorem, ut auctor quam. Sed id elit sapien. Mauris consequat ullamcorper lacus, sodales fringilla leo euismod eget. Aliquam luctus nulla eget varius eleifend. Vivamus convallis nulla vel urna aliquam, convallis scelerisque justo cursus.
-
-          Quisque feugiat leo ut nisl facilisis ornare. Integer gravida purus eget diam dictum finibus. Integer vel lacus eget nunc hendrerit tincidunt. Suspendisse ultricies arcu vitae odio cursus ullamcorper. Praesent fermentum, urna in aliquet pretium, dolor magna vulputate neque, ut finibus ex tortor ut orci. Aliquam et cursus ipsum, placerat pharetra nisl. Quisque sodales cursus finibus. Quisque vehicula lorem est, sit amet scelerisque arcu volutpat quis. Praesent luctus quam a nulla convallis, vel vehicula quam auctor. Aliquam fermentum diam bibendum ligula condimentum pellentesque. Nunc eget nisl elit. Etiam et luctus mi. Sed finibus pretium purus, in varius dui pharetra vel. Morbi id quam mauris. Vivamus elementum ornare enim nec posuere.
-
-          Aliquam pretium massa ac tempus mollis. Suspendisse vel lacus dolor. Donec vulputate viverra magna, ac accumsan ante pretium at. Donec auctor sagittis enim at dapibus. Aenean tempor sodales ante, ullamcorper feugiat urna fermentum nec. Mauris eu ultrices libero. Nulla quis volutpat mauris. Proin consectetur justo enim, non mollis augue feugiat non. Etiam malesuada erat et turpis fringilla iaculis. Vestibulum nec consequat tortor. Proin dignissim consequat sagittis. Praesent porttitor a mauris non mattis. Sed ultricies, augue in efficitur feugiat, nulla quam consectetur enim, ac pretium libero tellus non leo. Vivamus mollis, arcu vitae consequat gravida, quam arcu sagittis urna, eu viverra magna lorem id nunc.
-        </p>
+<br><br><br>
+        <?php
 
 
+        // Tilkoblingsinformasjon
+        $tjener = "localhost";
+        $brukernavn = "root";
+        $passord = "";
+        $database = "comments";
+
+        // Opprette en kobling
+        $kobling = new mysqli($tjener, $brukernavn, $passord, $database);
+
+        // Sjekk om koblingen virker
+        if ($kobling->connect_error) {
+            die("Noe gikk galt: " . $kobling->connect_error);
+        } else {
+
+        }
+
+        // Angi UTF-8 som tegnsett
+        $kobling->set_charset("utf8");
+
+
+if(isset($_POST["leggtil"])) {
+   // Lagrer skjemafeltene i enklere navn
+   $name = $_POST["name"];
+   $comment = $_POST["comment"];
+
+   $sql = "INSERT INTO comments (name, comment) VALUES ('$name', '$comment')";
+
+
+   if($kobling->query($sql)) {
+       echo "Spørringen $sql ble gjennomført.";
+   } else {
+       echo "Noe gikk galt med spørringen $sql ($kobling->error).";
+   }
+
+ }
+        ?>
+        <script>
+                //thank you god. I promise never to use PHP again
+                if ( window.history.replaceState ) {
+                window.history.replaceState( null, null, window.location.href );
+                }
+              </script>
+
+<form  method='post'>
+
+  Navn: <br>
+  <input type="text" name="name"><br><br>
+  Kommentar: <br>
+  <textarea name='comment'></textarea><br>
+  <input type='submit'  name='leggtil' value="Legg til"><br>
+</form>
+
+
+<?php
+$sql = "SELECT * FROM comments ";
+$resultat = $kobling->query($sql);
+
+echo "<table>"; // Starter tabellen
+echo "<tr>"; // Lager en rad med overskrifter
+    echo "<th></th>";
+    echo "<th></th>";
+echo "</tr>";
+
+while($rad = $resultat->fetch_assoc()) {
+  $name = $rad["name"];
+  $comment = $rad["comment"];
+
+    echo "<tr>";
+       echo "<td>$name</td>";
+       echo "<td>$comment</td>";
+    echo "</tr>";
+
+}
+
+echo "</table>"; // Avslutter tabellen
+
+?>
       </div>
       <div class="footer">
         <h1> Dette er en UOFFISIEL Fanside laget som et skoleprosjekt. </h1>
---
+++ scripts/sql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE comments (
+
+  id int(11) not null AUTO_INCREMENT PRIMARY KEY,
+  name varchar(128) not null,
+  comment TEXT not null
+
+
+
+);
