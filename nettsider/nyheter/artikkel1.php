f2748c322bd8a2fbb70bf1203c81403a40a5b3ea
Tord Sætermo <tord.saetermo@users.noreply.github.com>
2019-03-26 08:44:26 +0100

    artikkel 1

--- nettsider/nyheter/artikkel1.php
+++ nettsider/nyheter/artikkel1.php
@@ -1,36 +1,44 @@
-
-
-
 <!doctype.html>
 <html>
   <head>
     <link rel="stylesheet" href="../../styles/stilark.css">
     <meta charset="utf-8">
     <title> Rosenborg Ballklub </title>
-    <link rel="icon" href="../../bilder/rosenborg.png">
+    <link rel="icon" href="../../bilder/rbk.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
     <div class="innpakning">
-      <div class="header">
-        <div class="logo">
-          <img class="logo1" src="../../bilder/rosenborg.png" width="200px" height="200px">
-        </div>
-        <div class="title">
+      <div class="topptekst">
+          <img src="../bilder/rosenborg.png" width="300px" height="auto">
           <h1> ROSENBORG BALLKLUB </h1>
+      </div>
+      <!-- Nedfallsmeny fra w3schools (https://www.w3schools.com/howto/howto_css_dropdown.asp) begynner-->
+      <div class="meny1">
+        <div class="dropdown">
+          <button class="dropbtn"> MENY </button>
+          <div class="dropdown-content">
+            <a href="../index.php"> HJEM </a>
+            <a class="meny_active" href="../nettsider/nyheter.php"> NYHETER </a>
+            <a href="../nettsider/tropp.php"> TROPP </a>
+            <a href="../nettsider/tabell.php"> TABELL/KAMPER </a>
+            <a href="../nettsider/om_rbk.php"> OM RBK </a>
+          </div>
         </div>
       </div>
-      <div class="meny">
+      <!-- Nedfallsmeny fra w3schools (https://www.w3schools.com/howto/howto_css_dropdown.asp) slutter -->
+      <!-- Meny som blir på topen av side fra w3schools (https://www.w3schools.com/howto/howto_css_sticky_element.asp) begynner -->
+      <div class="meny2">
         <ul>
           <li><a href="../../index.php"> HJEM </a></li>
-          <li><a href="../nyheter.php"> NYHETER </a></li>
-          <li><a href="../tropp.php"> TROPP </a></li>
-          <li><a href="../sponsorer.php"> SPONSORER </a></li>
-          <li><a href="../tabell.php"> TABELL/KAMPER </a></li>
-          <li><a href="../om_rbk.php"> OM RBK </a></li>
+          <li><a class="meny_active" href="../nettsider/nyheter.php"> NYHETER </a></li>
+          <li><a href="../nettsider/tropp.php"> TROPP </a></li>
+          <li><a href="../nettsider/tabell.php"> TABELL/KAMPER </a></li>
+          <li><a href="../nettsider/om_rbk.php"> OM RBK </a></li>
         </ul>
       </div>
+      <!-- Meny som blir på topen av side fra w3schools (https://www.w3schools.com/howto/howto_css_sticky_element.asp) slutter -->
       <div class="innhold">
 <br><br>
 <iframe src="http://www.rbk.no/nyheter/setter-kursen-for-marbella" height="250%" width="70%" style="border:none;"></iframe>
