efc737043a9bb236ce2e5a5c7e69ab852128d97d
jakob1604 <48098435+jakob1604@users.noreply.github.com>
2019-03-26 08:36:25 +0100

    css artikkel

--- nettsider/nyheter/artikkel1.php
+++ nettsider/nyheter/artikkel1.php
@@ -9,7 +9,7 @@
     <title> Rosenborg Ballklub </title>
     <link rel="icon" href="../../bilder/rosenborg.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
     <div class="innpakning">
@@ -122,7 +122,7 @@ if(isset($_POST["leggtil"])) {
 
       </div>
       <div class="bunntekst">
-        <img src="../bilder/rosenborg.png" alt="ROSENBORG" width="100" height="auto">
+        <img src="../../bilder/rosenborg.png" alt="ROSENBORG" width="100" height="auto">
         <h5> Velkommen til en UOFFISIELL nettside om Rosenborg Ballklub laget som et skoleprosjekt i Informasjonstaknologi 1. For å komme til Rosenborg Ballklubs faktiske nettside, klikk <a href="http://www.rbk.no/" target="_blank">her</a>. </h5>
         <p> Du kan nå RBK på: </p>
         <div class="sosialemedier">
