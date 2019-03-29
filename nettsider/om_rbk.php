df9ded8216e47df19a9a07129ecf5db2dde073b1
jakob1604 <48098435+jakob1604@users.noreply.github.com>
2019-03-29 13:32:04 +0100

    bakgrunnsbilde

--- nettsider/om_rbk.php
+++ nettsider/om_rbk.php
@@ -39,35 +39,44 @@
         </ul>
       </div>
       <!-- Meny som blir på topen av side fra w3schools (https://www.w3schools.com/howto/howto_css_sticky_element.asp) slutter -->
-      <div class="innhold">
-      <h9>  <h10> Daglig leder og arrangementssjef: </h10><br>
-        Tove Moe Dyrhaug  - 481 80 740  - tove.dyrhaug@rbk.no <br>
+<div class="background">
 
-      <h10>  Sportslig avdeling A-lag  </h10><br>
+
+      <div class="innhold_om_rbk">
+        <br><br>
+<h2>OM RBK</h2>
+<h3>ANSATTE</h3>
+<br>
+
+
+      <h9>  <h10> Daglig leder og arrangementssjef: </h10><br>
+        Tove Moe Dyrhaug  - 481 80 740  - tove.dyrhaug@rbk.no <br><br>
+
+      <h3>  Sportslig avdeling A-lag  </h3><br><br>
         <h10>Hovedtrener: </h10> <br>
-        Eirik Horneland <br>
+        Eirik Horneland <br><br>
         <h10>Assistenttrener: </h10> <br>
         Karl Oskar Emberland <br>
-        Trond Henriksen <br>
+        Trond Henriksen <br><br>
         <h10>Keepertrener/logistikkansvarlig:  </h10><br>
-        Jørn Jamtfall - 915 91 001- jorn.jamtfall@rbk.no <br>
+        Jørn Jamtfall - 915 91 001- jorn.jamtfall@rbk.no <br><br>
         <h10>Sportslig leder: </h10> <br>
-        Stig Inge Bjørnebye <br>
+        Stig Inge Bjørnebye <br><br>
         <h10>Med. koordinator fysioterapeut/osteopat:  </h10><br>
-        Haakon Schwabe,  - 959 59 539 - haakon.schwabe@rbk.no <br>
+        Haakon Schwabe,  - 959 59 539 - haakon.schwabe@rbk.no <br><br>
         <h10>Fysioterapeuter: </h10> <br>
         Christian Thorbjørnsen - christian.thorbjornsen@rbk.no <br>
-        Eirik Nordtvedt - 926 84 106 - eirik.nordtvedt@rbk.no <br>
+        Eirik Nordtvedt - 926 84 106 - eirik.nordtvedt@rbk.no <br><br>
         <h10>Sjefspeider: </h10> <br>
-        Hallvar Thoresen <br>
+        Hallvar Thoresen <br><br>
         <h10>Trener:  </h10><br>
-        Vebjørn Rodal <br>
+        Vebjørn Rodal <br><br>
         <h10>Materialforvalter:  </h10><br>
-        Gunnar Stene - 911 74 133 - gunnar.stene@rbk.no <br>
+        Gunnar Stene - 911 74 133 - gunnar.stene@rbk.no <br><br>
         <h10>Kostholdsekspert: </h10> <br>
-        Henning Svendsen <br>
+        Henning Svendsen <br><br>
         <h10>Lege: </h10> <br>
-        Reidar Due <br>
+        Reidar Due <br><br>
 
 
 
@@ -75,26 +84,27 @@
 
 
 
-        <h10>Økonomi, eiendom og stadion</h10>  <br>
+        <h3>Økonomi, eiendom og stadion</h3>  <br><br>
       <h10>  Leder:</h10>  <br>
-        Roar Munkvold - 916 61 563 - roar.munkvold@rbk.no <br>
+        Roar Munkvold - 916 61 563 - roar.munkvold@rbk.no <br><br>
       <h10>  Økonomimedarbeidere:</h10>  <br>
         Bergljot Angen - bergljot.angen@rbk.no <br>
-        Maren Rognhaug Nybrodal - maren.nybrodal@rbk.no <br>
+        Maren Rognhaug Nybrodal - maren.nybrodal@rbk.no <br><br>
       <h10>  Driftsleder:</h10>  <br>
-        Per Haugnes - 913 38 380 - per.haugnes@rbk.no <br>
+        Per Haugnes - 913 38 380 - per.haugnes@rbk.no <br><br>
       <h10>  Banemester:</h10>  <br>
-        Anders Øien - 996 21 200 - anders.oien@rbk.no <br>
+        Anders Øien - 996 21 200 - anders.oien@rbk.no <br><br>
       <h10>  Driftsmedarbeider:</h10>  <br>
-        Martin Tinnen <br>
+        Martin Tinnen <br><br>
 
-      <h10>  Salg </h10> <br>
       <h10>  Salgssjef:</h10>  <br>
-        Harald Lundemo - 902 20 899 - harald.lundemo@rbk.no <br>
+        Harald Lundemo - 902 20 899 - harald.lundemo@rbk.no <br><br>
       <h10>  Salg: </h10> <br>
         Bjørn Kottum - 951 64 364 - bjorn.kottum@rbk.no <br>
         Vegard Selven - 991 65 777 - vegard.selven@rbk.no <br>
-        Camilla Herstad - 924 46 418 - camilla.herstad@rbk.no <br> </h9>
+        Camilla Herstad - 924 46 418 - camilla.herstad@rbk.no <br><br> </h9>
+        <br><br>
+      </div>
       </div>
       <div class="bunntekst">
         <img src="../bilder/rosenborg.png" alt="ROSENBORG" width="100" height="auto">
