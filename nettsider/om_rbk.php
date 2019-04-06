8c70e2d3f9a5fc2554b27943d8abe1e0f1ed32eb
jakob1604 <48098435+jakob1604@users.noreply.github.com>
2019-04-06 14:58:12 +0200

    Oppdatering dokumentasjon Jakob

--- nettsider/om_rbk.php
+++ nettsider/om_rbk.php
@@ -6,6 +6,7 @@
     <title> Rosenborg Ballklub </title>
     <link rel="icon" href="../bilder/rbk.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Sosiale-media-ikon fra w3schools: https://www.w3schools.com/howto/howto_css_social_media_buttons.asp -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
@@ -14,7 +15,7 @@
           <img src="../bilder/rosenborg.png" width="300px" height="auto">
           <h1> ROSENBORG BALLKLUB </h1>
       </div>
-      <!-- Nedfallsmeny fra w3schools (https://www.w3schools.com/howto/howto_css_dropdown.asp) begynner-->
+      <!-- Nedfallsmeny fra w3schools (https://www.w3schools.com/howto/howto_css_dropdown.asp) begynner -->
       <div class="meny1">
         <div class="dropdown">
           <button class="dropbtn"> MENY </button>
@@ -43,79 +44,80 @@
 
 
       <div class="innhold_om_rbk">
-        <br><br>
-<h2>OM RBK</h2>
-<h3>ANSATTE</h3>
-<br>
+        <br>
+        <br>
+        <h2>OM RBK</h2>
+        <h3>ANSATTE</h3>
+        <br>
+        <h9>  <h10> Daglig leder og arrangementssjef: </h10><br>
+                Tove Moe Dyrhaug  - 481 80 740  - tove.dyrhaug@rbk.no <br><br>
 
-
-      <h9>  <h10> Daglig leder og arrangementssjef: </h10><br>
-        Tove Moe Dyrhaug  - 481 80 740  - tove.dyrhaug@rbk.no <br><br>
-
-      <h3>  Sportslig avdeling A-lag  </h3><br><br>
-        <h10>Hovedtrener: </h10> <br>
-        Eirik Horneland <br><br>
-        <h10>Assistenttrener: </h10> <br>
-        Karl Oskar Emberland <br>
-        Trond Henriksen <br><br>
-        <h10>Keepertrener/logistikkansvarlig:  </h10><br>
-        Jørn Jamtfall - 915 91 001- jorn.jamtfall@rbk.no <br><br>
-        <h10>Sportslig leder: </h10> <br>
-        Stig Inge Bjørnebye <br><br>
-        <h10>Med. koordinator fysioterapeut/osteopat:  </h10><br>
-        Haakon Schwabe,  - 959 59 539 - haakon.schwabe@rbk.no <br><br>
-        <h10>Fysioterapeuter: </h10> <br>
-        Christian Thorbjørnsen - christian.thorbjornsen@rbk.no <br>
-        Eirik Nordtvedt - 926 84 106 - eirik.nordtvedt@rbk.no <br><br>
-        <h10>Sjefspeider: </h10> <br>
-        Hallvar Thoresen <br><br>
-        <h10>Trener:  </h10><br>
-        Vebjørn Rodal <br><br>
-        <h10>Materialforvalter:  </h10><br>
-        Gunnar Stene - 911 74 133 - gunnar.stene@rbk.no <br><br>
-        <h10>Kostholdsekspert: </h10> <br>
-        Henning Svendsen <br><br>
-        <h10>Lege: </h10> <br>
-        Reidar Due <br><br>
-
-
+              <h3>  Sportslig avdeling A-lag  </h3><br><br>
+                <h10>Hovedtrener: </h10> <br>
+                Eirik Horneland <br><br>
+                <h10>Assistenttrener: </h10> <br>
+                Karl Oskar Emberland <br>
+                Trond Henriksen <br><br>
+                <h10>Keepertrener/logistikkansvarlig:  </h10><br>
+                Jørn Jamtfall - 915 91 001- jorn.jamtfall@rbk.no <br><br>
+                <h10>Sportslig leder: </h10> <br>
+                Stig Inge Bjørnebye <br><br>
+                <h10>Med. koordinator fysioterapeut/osteopat:  </h10><br>
+                Haakon Schwabe,  - 959 59 539 - haakon.schwabe@rbk.no <br><br>
+                <h10>Fysioterapeuter: </h10> <br>
+                Christian Thorbjørnsen - christian.thorbjornsen@rbk.no <br>
+                Eirik Nordtvedt - 926 84 106 - eirik.nordtvedt@rbk.no <br><br>
+                <h10>Sjefspeider: </h10> <br>
+                Hallvar Thoresen <br><br>
+                <h10>Trener:  </h10><br>
+                Vebjørn Rodal <br><br>
+                <h10>Materialforvalter:  </h10><br>
+                Gunnar Stene - 911 74 133 - gunnar.stene@rbk.no <br><br>
+                <h10>Kostholdsekspert: </h10> <br>
+                Henning Svendsen <br><br>
+                <h10>Lege: </h10> <br>
+                Reidar Due <br><br>
 
 
 
 
 
-        <h3>Økonomi, eiendom og stadion</h3>  <br><br>
-      <h10>  Leder:</h10>  <br>
-        Roar Munkvold - 916 61 563 - roar.munkvold@rbk.no <br><br>
-      <h10>  Økonomimedarbeidere:</h10>  <br>
-        Bergljot Angen - bergljot.angen@rbk.no <br>
-        Maren Rognhaug Nybrodal - maren.nybrodal@rbk.no <br><br>
-      <h10>  Driftsleder:</h10>  <br>
-        Per Haugnes - 913 38 380 - per.haugnes@rbk.no <br><br>
-      <h10>  Banemester:</h10>  <br>
-        Anders Øien - 996 21 200 - anders.oien@rbk.no <br><br>
-      <h10>  Driftsmedarbeider:</h10>  <br>
-        Martin Tinnen <br><br>
 
-      <h10>  Salgssjef:</h10>  <br>
-        Harald Lundemo - 902 20 899 - harald.lundemo@rbk.no <br><br>
-      <h10>  Salg: </h10> <br>
-        Bjørn Kottum - 951 64 364 - bjorn.kottum@rbk.no <br>
-        Vegard Selven - 991 65 777 - vegard.selven@rbk.no <br>
-        Camilla Herstad - 924 46 418 - camilla.herstad@rbk.no <br><br> </h9>
-        <br><br>
-      </div>
+
+                <h3>Økonomi, eiendom og stadion</h3>  <br><br>
+              <h10>  Leder:</h10>  <br>
+                Roar Munkvold - 916 61 563 - roar.munkvold@rbk.no <br><br>
+              <h10>  Økonomimedarbeidere:</h10>  <br>
+                Bergljot Angen - bergljot.angen@rbk.no <br>
+                Maren Rognhaug Nybrodal - maren.nybrodal@rbk.no <br><br>
+              <h10>  Driftsleder:</h10>  <br>
+                Per Haugnes - 913 38 380 - per.haugnes@rbk.no <br><br>
+              <h10>  Banemester:</h10>  <br>
+                Anders Øien - 996 21 200 - anders.oien@rbk.no <br><br>
+              <h10>  Driftsmedarbeider:</h10>  <br>
+                Martin Tinnen <br><br>
+
+              <h10>  Salgssjef:</h10>  <br>
+                Harald Lundemo - 902 20 899 - harald.lundemo@rbk.no <br><br>
+              <h10>  Salg: </h10> <br>
+                Bjørn Kottum - 951 64 364 - bjorn.kottum@rbk.no <br>
+                Vegard Selven - 991 65 777 - vegard.selven@rbk.no <br>
+                Camilla Herstad - 924 46 418 - camilla.herstad@rbk.no <br><br> </h9>
+                <br><br>
+              </div>
       </div>
       <div class="bunntekst">
         <img src="../bilder/rosenborg.png" alt="ROSENBORG" width="100" height="auto">
         <h5> VelkOmmen til en UOFFISIELL nettside Om ROsenbOrg Ballklub laget sOm et skOleprOsjekt i InfOrmasjOnsteknOlOgi 1. FOr å kOmme til ROsenbOrg Ballklubs faktiske nettside, klikk <a href="http://www.rbk.no/" target="_blank">her</a>. </h5>
         <p> Du kan nå RBK på: </p>
+        <!-- Sosiale-medier-linker fra w3schools: https://www.w3schools.com/howto/howto_css_social_media_buttons.asp begynner -->
         <div class="sosialemedier">
           <a href="https://nb-no.facebook.com/rosenborg/" target="_blank" class="fa fa-facebook"></a>
           <a href="https://twitter.com/RBKfotball?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" class="fa fa-twitter"></a>
           <a href="https://www.youtube.com/channel/UCMzBr0zvB2uhUTRvnSFzwIw" target="_blank" class="fa fa-youtube"></a>
           <a href="https://www.instagram.com/rosenborgballklub/?hl=nb" target="_blank" class="fa fa-instagram"></a>
         </div>
+        <!-- Sosiale-medier-linker fra w3schools: https://www.w3schools.com/howto/howto_css_social_media_buttons.asp slutter -->
         <p> E-pOst: infO@rbk.nO eller TelefOn: 73822100 </p>
       </div>
     </div>
